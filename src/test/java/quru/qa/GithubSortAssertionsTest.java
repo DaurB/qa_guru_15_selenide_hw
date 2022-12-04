6e4aad5de5c63bfa5205f18e7a25af55a9bd75ad
dauren <biboldauren@gmail.com>
2022-12-05 05:09:54 +0600

    added test

--- src/test/java/quru/qa/GithubSortAssertionsTest.java
+++ src/test/java/quru/qa/GithubSortAssertionsTest.java
@@ -1,4 +1,30 @@
 package quru.qa;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
 public class GithubSortAssertionsTest {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void sortAssertionsTest() {
+        open("https://github.com/");
+        $(".header-search-input").setValue("selenide").submit();
+        $$(".repo-list li").first().$("a").click();
+        $("#wiki-tab").click();
+        $(".wiki-pages-box .wiki-more-pages-link button").click();
+        $(byText("SoftAssertions")).click();
+        $("#user-content-3-using-junit5-extend-test-class").ancestor("h4").shouldHave(text("Using JUnit5 extend test class:"));
+    }
+
 }
